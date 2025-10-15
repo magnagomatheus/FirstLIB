3279a8a02c25cfdb18746e91d0e8b4df3d72c432
Matheus De Oliveira Magnago <20232BSI0115@cefetes.br>
2025-10-15 10:31:07 -0300

    pytest working now

--- test_lib/test_lib_main.py
+++ test_lib/test_lib_main.py
@@ -1,5 +1,5 @@
 import pytest
-from PrimeiroLIB import add, subtract, multiply, divide
+from PrimeiroLIB.operations import add, subtract, multiply, divide
 
 def test_add():
     assert add(2, 3) == 5
